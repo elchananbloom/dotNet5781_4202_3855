86e5713f77b7a6935ac8af41e08384463fffc961
elchanan bloom <elchananbloom12@gmail.com>
2021-02-08 09:39:40 +0200

    added the timeSpan problem

--- ObjectToXml/ObjectToXml.cs
+++ ObjectToXml/ObjectToXml.cs
@@ -8,6 +8,7 @@ using DAL;
 using DALAPI;
 using DO;
 using System.Xml.Linq;
+using System.Xml;
 
 namespace ObjectToXml
 {
@@ -17,17 +18,20 @@ namespace ObjectToXml
         {
             XMLTools.SaveListToXMLSerializer<BusLineDAO>(DataSource.DataSource.BusLinesList, @"BusLines.xml");
             XMLTools.SaveListToXMLSerializer<BusDAO>(DataSource.DataSource.BussesList, @"Busses.xml");
-            XMLTools.SaveListToXMLSerializer<CoupleStationInRowDAO>(DataSource.DataSource.CoupleStationInRowList, @"CoupleStationInRow.xml");
+           // XMLTools.SaveListToXMLSerializer<CoupleStationInRowDAO>(DataSource.DataSource.CoupleStationInRowList, @"CoupleStationInRow.xml");
             XMLTools.SaveListToXMLSerializer<StationLineDAO>(DataSource.DataSource.StationLinesList, @"StationLines.xml");
             XMLTools.SaveListToXMLSerializer<StationDAO>(DataSource.DataSource.StationsList, @"Stations.xml");
-            //foreach(var item in DataSource.DataSource.CoupleStationInRowList)
-            //{
-            //    XElement coupleStation = new XElement("CoupleStationInRow", new XElement("StationNumberOne", item.StationNumberOne),
-            //                       new XElement("StationNumberTwo", item.StationNumberTwo),
-            //                       new XElement("Distance", item.Distance),
-            //                       new XElement("AverageTravelTime", item.AverageTravelTime.ToString()));
-            //    XMLTools.SaveListToXMLElement(item, @"CoupleStationInRow.xml");
-            //}
+            XElement myElement = new XElement("CoupleStationInRowList");
+            foreach (var item in DataSource.DataSource.CoupleStationInRowList)
+            {
+                XElement coupleStation = new XElement("CoupleStationInRow", new XElement("StationNumberOne", item.StationNumberOne),
+                                   new XElement("StationNumberTwo", item.StationNumberTwo),
+                                   new XElement("Distance", item.Distance),
+                                   new XElement("AverageTravelTime", XmlConvert.ToString(item.AverageTravelTime)));
+                myElement.Add(coupleStation);
+            }
+            //XmlConvert.ToTimeSpan()
+            XMLTools.SaveListToXMLElement(myElement, @"CoupleStationInRow.xml");
             //XMLTools.SaveListToXMLElement()
         }
     }
